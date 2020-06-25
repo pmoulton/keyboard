1464bdfa55eba3c5f0ed2347817ba1cdeb6c5bb9
Paul Moulton <pmoulton2@gmail.com>
2020-06-26 01:27:32 +0900

    Add dual-function control/esc

--- ergodox/ergodox_ez_mac_os_python_source/keymap.c
+++ ergodox/ergodox_ez_mac_os_python_source/keymap.c
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LBRACKET,                                    KC_RBRACKET,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_GRAVE,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_MINUS,                                       KC_UNDS,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    KC_CAPSLOCK,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    LCTL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSPO,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_TAB,                                         KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSPC,
     MO(2),          KC_MAC_CUT,     KC_MAC_COPY,    KC_MAC_PASTE,   MO(1),                                                                                                          MO(1),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, MO(2),
                                                                                                     ST_MACRO_0,     KC_DELETE,      KC_RGUI,        ST_MACRO_1,
                                                                                                                     KC_LALT,        KC_RALT,
-                                                                                    KC_LGUI,        KC_BSPACE,      KC_CAPSLOCK,    KC_CAPSLOCK,    KC_ENTER,       KC_SPACE
+                                                                                    KC_LGUI,        KC_BSPACE,      LCTL_T(KC_ESCAPE),RCTL_T(KC_ESCAPE),KC_ENTER,       KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_MEDIA_PLAY_PAUSE,                                KC_AUDIO_MUTE,  KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F12,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_MEDIA_NEXT_TRACK,                                KC_AUDIO_VOL_UP,ST_MACRO_2,     KC_PGUP,        KC_HOME,        ST_MACRO_3,     KC_TRANSPARENT, KC_F11,
+    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_MEDIA_PLAY_PAUSE,                                KC_AUDIO_MUTE,  KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_MEDIA_NEXT_TRACK,                                KC_AUDIO_VOL_UP,ST_MACRO_2,     KC_PGUP,        KC_HOME,        ST_MACRO_3,     KC_TRANSPARENT, KC_F12,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_TRANSPARENT,                                                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,                                KC_AUDIO_VOL_DOWN,KC_TRANSPARENT, KC_PGDOWN,      KC_END,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_0,           KC_0,           KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
