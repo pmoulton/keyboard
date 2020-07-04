5497fc7f7a793d9d4f9d534f00714c8b2ffb88ca
Paul Moulton <pmoulton2@gmail.com>
2020-07-04 17:04:17 +0900

    FILL IN

--- ergodox/ergodox_ez_mac_os_python_source/keymap.c
+++ ergodox/ergodox_ez_mac_os_python_source/keymap.c
@@ -46,6 +46,7 @@ enum custom_keycodes {
   ST_MACRO_5,
   ST_MACRO_6,
   ST_MACRO_7,
+  ST_MACRO_8,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_MINUS,                                       KC_UNDS,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     LCTL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSPO,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           LCTL_T(KC_ESCAPE),                                KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSPC,
-    MO(2),          KC_MAC_CUT,     KC_MAC_COPY,    KC_MAC_PASTE,   MO(1),                                                                                                          MO(1),          ST_MACRO_1,     KC_TRANSPARENT, KC_TRANSPARENT, MO(2),
-                                                                                                    ST_MACRO_0,     KC_DELETE,      ST_MACRO_2,     ST_MACRO_3,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
+    MO(2),          KC_MAC_CUT,     KC_MAC_COPY,    KC_MAC_PASTE,   MO(1),                                                                                                          MO(1),          ST_MACRO_2,     KC_TRANSPARENT, KC_TRANSPARENT, MO(2),
+                                                                                                    ST_MACRO_0,     KC_DELETE,      ST_MACRO_3,     ST_MACRO_4,
+                                                                                                                    ST_MACRO_1,     KC_RALT,
                                                                                     KC_LGUI,        KC_BSPACE,      KC_LALT,        KC_RGUI,        KC_ENTER,       KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_MEDIA_PLAY_PAUSE,                                KC_AUDIO_MUTE,  KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_MEDIA_NEXT_TRACK,                                KC_AUDIO_VOL_UP,ST_MACRO_4,     KC_PGUP,        KC_HOME,        ST_MACRO_5,     KC_TRANSPARENT, KC_F12,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_MEDIA_NEXT_TRACK,                                KC_AUDIO_VOL_UP,ST_MACRO_5,     KC_PGUP,        KC_HOME,        ST_MACRO_6,     KC_TRANSPARENT, KC_F12,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_TRANSPARENT,                                                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,                                KC_AUDIO_VOL_DOWN,KC_TRANSPARENT, KC_PGDOWN,      KC_END,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_0,           KC_0,           KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -72,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RESET,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT,                                                                 ST_MACRO_6,     KC_TRANSPARENT, KC_TRANSPARENT, ST_MACRO_7,     KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT,                                                                 ST_MACRO_7,     KC_TRANSPARENT, KC_TRANSPARENT, ST_MACRO_8,     KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, WEBUSB_PAIR,    KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -88,13 +89,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_RBRACKET));
+      SEND_STRING(SS_TAP(X_LBRACKET) SS_DELAY(100) SS_TAP(X_RBRACKET) SS_DELAY(100) SS_TAP(X_LEFT));
 
     }
     break;
     case ST_MACRO_1:
     if (record->event.pressed) {
-      SEND_STRING(SS_RALT(SS_TAP(X_SPACE)));
+      SEND_STRING(SS_LCTL(SS_TAP(X_B)) SS_DELAY(100) SS_TAP(X_N));
 
     }
     break;
@@ -106,29 +107,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     break;
     case ST_MACRO_3:
     if (record->event.pressed) {
-      SEND_STRING(SS_RSFT(SS_TAP(X_9)) SS_DELAY(100) SS_RSFT(SS_TAP(X_0)));
+      SEND_STRING(SS_RALT(SS_TAP(X_SPACE)));
 
     }
     break;
     case ST_MACRO_4:
     if (record->event.pressed) {
-      SEND_STRING(SS_RALT(SS_TAP(X_LEFT)));
+      SEND_STRING(SS_RSFT(SS_TAP(X_9)) SS_DELAY(100) SS_RSFT(SS_TAP(X_0)));
 
     }
     break;
     case ST_MACRO_5:
     if (record->event.pressed) {
-      SEND_STRING(SS_RALT(SS_TAP(X_RIGHT)));
+      SEND_STRING(SS_RALT(SS_TAP(X_LEFT)));
 
     }
     break;
     case ST_MACRO_6:
     if (record->event.pressed) {
-      SEND_STRING(SS_RALT(SS_TAP(X_LEFT)));
+      SEND_STRING(SS_RALT(SS_TAP(X_RIGHT)));
 
     }
     break;
     case ST_MACRO_7:
+    if (record->event.pressed) {
+      SEND_STRING(SS_RALT(SS_TAP(X_LEFT)));
+
+    }
+    break;
+    case ST_MACRO_8:
     if (record->event.pressed) {
       SEND_STRING(SS_RALT(SS_TAP(X_RIGHT)));
 
